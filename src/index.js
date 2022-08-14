af6c980f7005e942450d3743fd87386b9fe744f5
alok <alokpatle123@gmail.com>
2022-08-14 21:01:52 +0530

    mongo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://alok10:vralok10@cluster0.rntxqd0.mongodb.net/alok10-DB?retryWrites=true&w=majority"
 , {
    useNewUrlParser: true 
 }
