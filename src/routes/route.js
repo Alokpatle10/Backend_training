5b1201dac8b749377bd11c54db47fbc50781968d
alok <alokpatle123@gmail.com>
2022-08-17 17:44:31 +0530

    mongoo session 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const BookController = require("../controllers/userController");
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// ===========================================================
+router.post("/createBook", BookController.createBook);
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// ============================================================
 
-router.post("/createBook", BookController.createBook  )
+router.get("/bookList", BookController.bookList)
 
-router.get("/getBooksData", BookController.getBooksData)
+// =======================================================---==
+
+router.get("/getBooksInYear", BookController.getBooksInYear)
+
+// ==============================================================
+
+router.post("/getParticularBooks", BookController.getParticularBooks);
+
+// ===================================================================
+
+router.get("/getXINRBooks", BookController.getXINRBooks);
+
+// =========================================================================
+
+router.get("/getRandomBooks", BookController.getRandomBooks);
+
+
+// router.get("/getparticularBooks", UserController.getparticularBooks)
 
 module.exports = router;
