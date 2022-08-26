89a5d71cb0c2ad378c6341b7f4705e50d8b6301c
alok <alokpatle123@gmail.com>
2022-08-26 15:07:30 +0530

    middleware

---
+++ new.js
@@ -0,0 +1,12 @@
+getName();
+console.log(x)
+
+
+
+let x = 7;
+function getName (){
+  console.log("alok")
+}
+
+// getName();
+// console.log(x)
--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+const moment=require('moment')
+const time=moment()
+app.use(
+    function(req,res,next){
+        console.log("this is Global")
+        console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        console.log(req.ip);
+        console.log(req.Url)
+        next()
+    }
+)
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://alok10:vralok10@cluster0.rntxqd0.mongodb.net/vaishu-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -24,7 +36,6 @@ app.use (
         res.send({msg:"done"})
   }
   );
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
