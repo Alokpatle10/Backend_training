e4089447661a7172993db68ca9e5f61731c9739e
alok <alokpatle123@gmail.com>
2022-08-29 16:01:33 +0530

    jwt1

---
+++ src/controllers/practiceController.js
@@ -0,0 +1,15 @@
+
+// const practiceModel =require("../models/practice")
+
+// const practice1= async function(req,res){
+//     const data = req.body
+//     const crt = await practiceModel.create(data)
+//     res.send({msg:crt})
+// }
+
+// const getdata= async function(req,res){
+//     const data = await practiceModel.find({"authorName":"alok"})
+//     res.send({msg:data})
+// }
+// module.exports.practice1=practice1
+// module.exports.getdata=getdata
